47e4db75664b49b28e0e499092dfcc30ef79a01a
Jan Heuermann <jan@jotaen.net>
2019-07-11 19:01:08 +0200

    Add `compose`

--- src/list-spec.rkt
+++ src/list-spec.rkt
@@ -11,4 +11,14 @@
     (check-equal? (empty? '(1 2 3)) #f)
   )
 
+  (test-case "compose"
+    (define (plus4 x) (+ x 4))
+    (define (times6 x) (* x 6))
+    (define (minus2 x) (- x 2))
+    (check-equal? ((compose identity) 'a) 'a)
+    (check-equal? ((compose identity identity identity) 'a) 'a)
+    (check-equal? ((compose plus4 times6 minus2) 3) 10)
+    (check-equal? ((compose minus2 times6 plus4) 3) 40)
+  )
+
 )
--- src/list.scm
+++ src/list.scm
@@ -1,1 +1,4 @@
 (define (empty? xs) (= 0 (length xs)))
+
+(define (compose . fs)
+  (lambda (x) (foldr (lambda (f x) (f x)) x fs)))
