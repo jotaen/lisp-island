faa2af3e99e378479d006aaa400e0e7ae08b31ec
Jan Heuermann <jan@jotaen.net>
2019-07-12 17:15:42 +0200

    Split groups into subgroups and checks

--- lib/scmunit.scm
+++ lib/scmunit.scm
@@ -2,43 +2,47 @@
 
 (define *scmunit-groups* ())
 
-(define-structure scmunit-result
+(define-structure scmunit-check
     predicate expression actual expected ok?)
 
 (define-structure scmunit-group
-    name items)
+    name checks groups)
 
 (define-syntax check
   (syntax-rules () ((_ predicate expression expected)
     (let* (
       (actual expression)
       (ok? (predicate actual expected)))
-        (make-scmunit-result 'predicate 'expression actual expected ok?)))))
+        (make-scmunit-check 'predicate 'expression actual expected ok?)))))
 
 (define-syntax test-group
     (syntax-rules () ((_ name items ...)
-        (make-scmunit-group name (list items ...)))))
+        (let ((is (list items ...)))
+            (make-scmunit-group
+                name
+                (filter (lambda (x) (scmunit-check? x)) is)
+                (filter (lambda (x) (scmunit-group? x)) is))))))
 
 (define-syntax test-group*
     (syntax-rules () ((_ name items ...)
         (set! *scmunit-groups* (append *scmunit-groups* (list (test-group name items ...)))))))
 
 (define (scmunit-run)
-    (define (header g) (string-append "# " (scmunit-group-name g)))
-    (define (check-verbose c)
+    (define (result-verbose c)
         (format #f
         "\n~A   :::   (~A ~A ~A)   >>>   ~A"
-        (scmunit-result-expression c)
-        (scmunit-result-predicate c)
-        (scmunit-result-actual c)
-        (scmunit-result-expected c)
-        (scmunit-result-ok? c)))
-    (define (check-quiet c) (if (scmunit-result-ok? c) "." "E"))
-    (define (stringify xs pref) (fold-right (lambda (x a) (cond
-        ((scmunit-group? x) (string-append a "\n" pref (header x) " " (stringify (list (scmunit-group-items x)) (string-append pref "  "))))
-        ((scmunit-result? x) (string-append a (check-quiet x)))
-        ((list? x) (string-append a (stringify x pref)))
-        (error "Wrong type!")
+        (scmunit-check-expression c)
+        (scmunit-check-predicate c)
+        (scmunit-check-actual c)
+        (scmunit-check-expected c)
+        (scmunit-check-ok? c)))
+    (define (check-summary cs)
+        (fold-right (lambda (c a) (string-append a (if (scmunit-check-ok? c) "." "E"))) "" cs))
+    (define (group-summary g)
+        (string-append "# " (scmunit-group-name g) " " (check-summary (scmunit-group-checks g))))
+    (define (stringify xs pref) (fold-right (lambda (x a) (if
+        (list? x) (string-append a (stringify x pref))
+        (string-append a "\n" pref (group-summary x) " " (stringify (list (scmunit-group-groups x)) (string-append pref "  ")))
     )) "" xs))
     (begin
         (display (stringify *scmunit-groups* ""))
