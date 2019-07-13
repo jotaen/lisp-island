a1ce4474d42293e0f83c49832e9aeaa4799f9d28
Jan Heuermann <jan@jotaen.net>
2019-07-13 11:06:49 +0200

    Simplify list generator

--- src/list-spec.scm
+++ src/list-spec.scm
@@ -8,7 +8,7 @@
     (check eq? (empty? '(1 2 3)) #f)
   ))
 
-  (test-group "compose" ((lambda () 
+  (test-group "compose" (let () 
   (define (plus4 x) (+ x 4))
   (define (times6 x) (* x 6))
   (define (minus2 x) (- x 2))
@@ -17,6 +17,6 @@
     (check eq? ((compose identity identity identity) 'a) 'a)
     (check eq? ((compose plus4 times6 minus2) 3) 10)
     (check eq? ((compose minus2 times6 plus4) 3) 40)
-  ))))
+  )))
 
 ))
